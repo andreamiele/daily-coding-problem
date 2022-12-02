e95228eccefe9bcf74eb5fe3bb42caf3eb878902
Andrea Miele <andrea.miele.pro@gmail.com>
2022-12-02 06:56:57 +0100

    change url and day number

--- AoC2022/day2/day2.py
+++ AoC2022/day2/day2.py
@@ -1,4 +1,4 @@
-# Advent of Code 2022 Day 1 : https://adventofcode.com/2022/day/1
+# Advent of Code 2022 Day 2 : https://adventofcode.com/2022/day/2
 
 ##################### Instructions #####################
 
