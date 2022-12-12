5faab40ebec07b4f436bbbb874e2d587dfef3c9f
Andrea Miele <84397367+andreamiele@users.noreply.github.com>
2022-12-12 13:05:35 +0100

    Add some context for deque

--- AoC2022/12/test.py
+++ AoC2022/12/test.py
@@ -1,4 +1,12 @@
-from collEctions import deque #Double end queue in really useful is this problem 
+from collections import deque #Double end queue in really useful is this problem 
+"""
+Deque (Doubly Ended Queue) in Python is implemented using the module “collections“. 
+Deque is preferred over a list in the cases where we need quicker append and pop operations from both the ends of the container, 
+as deque provides an O(1) time complexity for append and pop operations as compared to a list that provides O(n) time complexity.
+
+More information here: https://www.geeksforgeeks.org/deque-in-python/
+""""
+
 if __name__ == '__main__':
     import os
 
