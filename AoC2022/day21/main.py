9b39e89493616144baf3f20a4949b0b6d3ad7b1a
Andrea Miele <84397367+andreamiele@users.noreply.github.com>
2022-12-21 11:18:40 +0100

    day21

---
+++ AoC2022/day21/main.py
@@ -0,0 +1,47 @@
+#Part 1
+
+monkeys = {}
+
+tab = [line.strip() for line in open(0)]
+
+for obj in tab:
+    name, expr = obj.split(": ")
+    if expr.isdigit():
+        monkeys[name] = int(expr)
+    else:
+        left, op, right = expr.split()
+        if left in monkeys and right in monkeys:
+            monkeys[name] = eval(f"{monkeys[left]} {op} {monkeys[right]}")
+        else:
+            tab.append(obj)
+
+print(monkeys["root"])
+
+#Part 2
+import sympy
+
+monkeys = { "humn": sympy.Symbol("x") }
+
+tab = [line.strip() for line in open(0)]
+
+ops = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+}
+
+for obj in tab:
+    name, expr = obj.split(": ")
+    if name in monkeys: continue
+    if expr.isdigit():
+        monkeys[name] = sympy.Integer(expr)
+    else:
+        left, op, right = expr.split()
+        if left in monkeys and right in monkeys:
+            if name == "root":
+                print(sympy.solve(monkeys[left] - monkeys[right])[0])
+                break
+            monkeys[name] = ops[op](monkeys[left], monkeys[right])
+        else:
+            tab.append(obj)
